6ab48bba995d5442a8afc280a1171a8c4cfc29d6
전국표 (KookPyo Chun) <158117620+KOOKPYOCHUN@users.noreply.github.com>
2024-04-04 11:35:26 +0900

    Update Subscription in python.py

--- Python/Subscription in python.py
+++ Python/Subscription in python.py
@@ -11,5 +11,12 @@ def callback(data): ## 토픽을 받을 때마다 실행하게 하는 함수, X,
   print("Y : "data.y)
   print("Theta : ", data.theta)
 
+test_node.create_subscription(Pose, '/turtle1/pose', callback, 10) # test_node에서 create_subscription을 하기 형식으로 호출
+# test_node.create_subscription(<data_type>, '<topic_name>', <callback>, <QoS History>) , <QoS History> = 저장할 샘플의 수
+
+rp.spin_onca(test_node) # spin_once 명령을 통해 test_node를 rp.spin_onca(test_node)라고 지정, 해당 노드 구독 
+
 ## Note ##
 ros2 node list 사용하여 노드 생성되었는지 확인 가능
+spin_noce는 토픽을 한 번만 받는 명령
+spin은 토픽을 무한하게 구독
